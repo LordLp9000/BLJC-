1ea99deecdc48288543997daab5cec91279b172a
Laurent Scherrer <147433411+LordLp9000@users.noreply.github.com>
2025-09-30 08:40:54 +0200

    20 mit Newtonsoft.json

--- Aufgabe20/Program.cs
+++ Aufgabe20/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Aufgabe20
 {
@@ -81,14 +83,36 @@ namespace Aufgabe20
         {
             try
             {
+                JArray array = JArray.Parse(jsonData);
+
+                if (array.Count > 0)
+                {
+                    JObject joke = (JObject)array[0];
+                    return joke["text"]?.ToString();
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"JSON-Parsing Fehler: {ex.Message}");
+                return ExtrahiereWitzTextFallback(jsonData);
+            }
+        }
+
+        static string ExtrahiereWitzTextFallback(string jsonData)
+        {
+            try
+            {
                 int textStart = jsonData.IndexOf("\"text\":\"");
                 if (textStart == -1) return null;
 
-                textStart += 8;
-                int textEnd = jsonData.IndexOf("\",\"language\"", textStart);
+                textStart += 8; // Länge von "\"text\":\""
+                int textEnd = jsonData.IndexOf("\",", textStart);
                 if (textEnd == -1)
                 {
-                    textEnd = jsonData.IndexOf("\"", textStart);
+                    // Falls es das letzte Element ist, suche nach "}"
+                    textEnd = jsonData.IndexOf("\"}", textStart);
                     if (textEnd == -1) return null;
                 }
 
