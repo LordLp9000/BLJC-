fdc3fbf1ba6768dadbb1e57b652d7d7b460457c6
Laurent Scherrer <147433411+LordLp9000@users.noreply.github.com>
2025-09-30 06:58:21 +0200

    18

---
+++ Aufgabe18/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deine Eingabe: ");
+            string text = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                ZähleVokale(text);
+            }
+            else
+            {
+                Console.WriteLine("Kein Text eingegeben!");
+            }
+
+            Console.WriteLine("\nDrücken Sie eine beliebige Taste zum Beenden...");
+            Console.ReadKey();
+        }
+
+        static void ZähleVokale(string text)
+        {
+            char[] vokale = { 'a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü', 'A', 'E', 'I', 'O', 'U', 'Ä', 'Ö', 'Ü' };
+            Dictionary<char, int> vokalZähler = new Dictionary<char, int>();
+            int gesamtVokale = 0;
+
+            foreach (char vokal in vokale)
+            {
+                vokalZähler[vokal] = 0;
+            }
+
+            foreach (char zeichen in text)
+            {
+                if (vokalZähler.ContainsKey(zeichen))
+                {
+                    vokalZähler[zeichen]++;
+                    gesamtVokale++;
+                }
+            }
+
+            Console.WriteLine($"\nDein Text hat total {gesamtVokale} Vokale.");
+
+            foreach (var paar in vokalZähler)
+            {
+                if (paar.Value > 0)
+                {
+                    Console.WriteLine($"Der Buchstabe '{paar.Key}' kommt {paar.Value} mal vor.");
+                }
+            }
+        }
+    }
+}
